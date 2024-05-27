269c34073420363bfdb0195a299cb87d55672fb5
Vaishnav Ghenge <vaishnavghenge@gmail.com>
2024-05-27 10:16:07 +0530

    Update

--- src/Rest/routes/Meet.ts
+++ src/Rest/routes/Meet.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import { createMeet, joinMeet } from "../controllers/Meet";
+import { checkAuthToken } from "../validators/Auth";
 
 const router = express.Router();
+
+// router.use(checkAuthToken);
 
 router.get("/join", joinMeet);
 router.post("/create", createMeet);
